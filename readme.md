6a8d0697b4a20ba1229a90f0d110e59758d42cf8
Arshdeep Padda <aspadda@icloud.com>
2023-09-28 19:58:39 -0700

    Added mergesort

--- readme.md
+++ readme.md
@@ -1,3 +1,9 @@
 # Python Algorithms
 
-A working collection of various algorithms written in Python3. 
+A working collection of various algorithms written in Python. 
+
+**Table of Content**
+
+**Sorts**
+
+1. [Merge Sort](sorts/mergesort)
---
+++ sorts/mergesort/mergesort.py
@@ -0,0 +1,30 @@
+def mergesort(A):
+    if len(A) <= 1:
+        return A
+
+    midpoint = len(A) // 2
+    L = mergesort(A[:midpoint])
+    R = mergesort(A[midpoint:])
+
+    a = 0
+    l = 0
+    r = 0
+    while l < len(L) and r < len(R):
+        if L[l] < R[r]:
+            A[a] = L[l]
+            l += 1
+        else:
+            A[a] = R[r]
+            r += 1
+        a += 1
+    
+    while l < len(L):
+        A[a] = L[l]
+        l += 1
+        a += 1
+    while r < len(R):
+        A[a] = R[r]
+        r += 1
+        a += 1
+    
+    return A
